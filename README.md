d257a840dd613b6b5b398e7581735587513a9d31
Boyan Rabchev <boyan@rabchev.com>
2013-11-21 17:59:54 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-node-authority
-==============
+authority
+=========
---
+++ index.js
@@ -0,0 +1,3 @@
+/*jslint plusplus: true, devel: true, nomen: true, node: true, indent: 4, maxerr: 50 */
+
+module.exports = require("./lib/authority");
