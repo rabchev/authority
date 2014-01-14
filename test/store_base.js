2e0d3425d45eb5108a016a869e18df087ea72747
Boyan Rabchev <boyan@rabchev.com>
2014-01-15 00:58:11 +0200

    Added store_base tests.

--- test/store_base.js
+++ test/store_base.js
@@ -63,11 +63,80 @@ exports.getSuit = function (store) {
         it("should return error on existing", function (done) {
             store.setRules({
                 name: "allow_testuser_only",
-                description: "Should Throw.",
+                description: "Replace rule.",
                 condition: {}
             }, function (err) {
                 expect(err).to.be.ok;
                 expect(err.message).to.equal(Strings.ERR_RULE_EXISTS);
+                done();
+            });
+        });
+
+        it("replace rule", function (done) {
+            store.setRules({
+                name: "allow_testuser_only",
+                description: "Replaced Rule (Allow testuser Only).",
+                condition: function (fact, cb) {
+                    cb(null, fact && fact.username === "testuser");
+                }
+            }, true, function (err) {
+                expect(err).to.not.be.ok;
+                store.getRule("allow_testuser_only", function (err2, res) {
+                    expect(err2).to.not.be.ok;
+                    expect(res.description).to.equal("Replaced Rule (Allow testuser Only).");
+                    done();
+                });
+            });
+        });
+
+        it ("get all rule names", function (done) {
+            store.getRuleNames(0, 0, null, function (err, rules) {
+                expect(err).to.not.be.ok;
+                expect(rules.length).to.equal(4);
+                expect(rules[0].name).to.equal("allow_testuser_only");
+                expect(rules[3].name).to.equal("allow_specific_roles");
+                done();
+            });
+        });
+
+        it ("get first two rule names", function (done) {
+            store.getRuleNames(0, 2, null, function (err, rules) {
+                expect(err).to.not.be.ok;
+                expect(rules.length).to.equal(2);
+                expect(rules[0].name).to.equal("allow_testuser_only");
+                expect(rules[1].name).to.equal("allow_teenagers_only");
+                done();
+            });
+        });
+
+        it ("get last two rule names", function (done) {
+            store.getRuleNames(2, 2, null, function (err, rules) {
+                expect(err).to.not.be.ok;
+                expect(rules.length).to.equal(2);
+                expect(rules[0].name).to.equal("allow_managers_only");
+                expect(rules[1].name).to.equal("allow_specific_roles");
+                expect(rules[1].description).to.equal("Only members of Admins, Power Users and Contributers can pass.");
+                done();
+            });
+        });
+
+        it ("get filtered rule names", function (done) {
+            store.getRuleNames(0, 0, "*_only", function (err, rules) {
+                expect(err).to.not.be.ok;
+                expect(rules.length).to.equal(3);
+                expect(rules[0].name).to.equal("allow_testuser_only");
+                expect(rules[1].name).to.equal("allow_teenagers_only");
+                expect(rules[2].name).to.equal("allow_managers_only");
+                done();
+            });
+        });
+
+        it ("get first two filtered rule names", function (done) {
+            store.getRuleNames(0, 2, "*_only", function (err, rules) {
+                expect(err).to.not.be.ok;
+                expect(rules.length).to.equal(2);
+                expect(rules[0].name).to.equal("allow_testuser_only");
+                expect(rules[1].name).to.equal("allow_teenagers_only");
                 done();
             });
         });
